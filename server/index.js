a02d33b02ec8964953abe3abdb17f1abbc20de5d
Tusharjungli <trpanchaltr@gmail.com>
2025-04-04 20:29:12 +0530

    Added MongoDB with .env

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config(); // Load .env variables
+
 const app = express();
+
+app.use(express.json()); // Parse JSON bodies
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log("MongoDB error:", err));
 
 app.get("/", (req, res) => {
   res.send("Hello, Auto Spare Parts!");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
 });
